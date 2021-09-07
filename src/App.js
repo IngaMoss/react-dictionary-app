101c0557e5a7ba74bbd5d5b9d725ce84975def49
IngaMoss <83215456+IngaMoss@users.noreply.github.com>
2021-09-07 10:17:42 +0200

    Search engine created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
+import React from 'react';
 import logo from './dictionary.png';
 import './App.css';
+
+import Dictionary from './Dictionary';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Dictionary
-        </p>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Inga Mosienko</footer>
+      </div>
     </div>
   );
 }
